1c72c42f5bb7db7a08642650c7ff821ef94b2ea6
Mauro Romito <maurovelin@yahoo.com>
2020-10-18 01:18:30 +0200

    created the filter button in the nav bar

--- FlyersDisplayer/Views/FlyersDisplayerViewController.swift
+++ FlyersDisplayer/Views/FlyersDisplayerViewController.swift
@@ -17,6 +17,32 @@ class FlyersDisplayerViewController: UIViewController, Storyboarded  {
     static let storyboardName = "Main"
     static let storyboardId = "FlyersDisplayerViewController"
     
+    var filterButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupFilterButton()
+    }
+    
+    private func setupFilterButton() {
+        filterButton = UIButton(type: .custom)
+        filterButton.frame = CGRect(x: 0.0, y: 0.0, width: 25, height: 25)
+        let image = UIImage(named:"icon_eye")!.withRenderingMode(.alwaysTemplate)
+        filterButton.setImage(image, for: .normal)
+        filterButton.imageView?.contentMode = .scaleAspectFit
+        filterButton.tintColor = .systemBlue
+        filterButton.addTarget(self, action: #selector(didTapOnFilterButton(_:)), for: .touchUpInside)
+        let filterBarItem = UIBarButtonItem(customView: filterButton)
+        let currWidth = filterBarItem.customView?.widthAnchor.constraint(equalToConstant: 30)
+        currWidth?.isActive = true
+        let currHeight = filterBarItem.customView?.heightAnchor.constraint(equalToConstant: 30)
+        currHeight?.isActive = true
+        self.navigationItem.rightBarButtonItem = filterBarItem
+    }
+    
+    @objc private func didTapOnFilterButton(_ sender: Any){
+        
+    }
 }
 
 extension FlyersDisplayerViewController: FlyersDisplayerViewProtocol {
